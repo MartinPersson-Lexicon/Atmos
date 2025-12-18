da84472703365a7c5a2210c3ebc687d8ae92fb33
mojbeyzavi <mojbeyzavi@gmail.com>
2025-12-18 22:04:48 +0100

    Merged version with code from origin main

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import '../App.css'
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <nav>
+        <Link to="/">Home</Link>
+        {' | '}
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+  )
+  
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Card4OtherCities from "../components/Card4OtherCities/Card4OtherCities";
-
+import CardOneWidget from "../components/card-one/CardOneWidget";
 export default function Home() {
   // const [theme, setTheme] = useState("dark");
   // const [searchQuery, setSearchQuery] = useState("");
@@ -9,6 +9,11 @@ export default function Home() {
     <div className="homePage">
       <div className="homeLeft">
         <h1 className="homeTitle">Home page</h1>
+
+        <div className="left-column">
+          <CardOneWidget />
+        </div>
+        <div className="right-column"></div>
 
         {/* Card4 should be in the left-bottom area */}
         <div className="leftBottom">
