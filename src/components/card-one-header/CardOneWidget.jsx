c9b9196e034207f1282b10dbb645759ff0c1cff1
Alisher <alisher1kayumov@gmail.com>
2025-12-19 10:55:41 +0100

    Added cardtwowidget

---
+++ src/components/card-one-header/CardOneWidget.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardOneWidget = ({ theme, setTheme, searchQuery, setSearchQuery }) => {
+  // ...existing code from CardOneHeader...
+  return (
+    // ...existing JSX from CardOneHeader...
+    <div>CardOneWidget placeholder</div>
+  );
+};
+
+export default CardOneWidget;
